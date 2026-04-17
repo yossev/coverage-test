c6465315b22537d47cf64ba285b18da3224e1e13
synthpyt <synthpyt@gmail.com>
2026-04-17 17:48:07 +0200

    initial commit

---
+++ MyLib.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLib.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectResult()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+
+    // intentionally NOT testing Subtract so coverage is below 100%
+}
---
+++ MyLib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MyLib;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+}
